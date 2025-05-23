810df3b14f47544f2d7dc6aa5991c6403ad7e080
Filipe Mota <filipera3@gmail.com>
2025-05-23 11:30:44 -0300

    feat: add User entity with basic validations and prePersist

---
+++ src/main/java/com/ofilipecode/wish_list/model/User.java
@@ -0,0 +1,43 @@
+package com.ofilipecode.wish_list.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank
+    @Email
+    private String email;
+
+    @Column(nullable = false)
+    @NotBlank
+    private String password;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
