6d82d9e1bf9290c0078004f59238170d11ee6e8c
Filipe Mota <filipera3@gmail.com>
2025-05-26 14:28:19 -0300

    fix: change id type in repository

--- src/main/java/com/ofilipecode/wish_list/repository/UserRepository.java
+++ src/main/java/com/ofilipecode/wish_list/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package com.ofilipecode.wish_list.repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ofilipecode.wish_list.model.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByEmail(String email);
 }
