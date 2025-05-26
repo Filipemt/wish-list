77aee277d89a69fd75ca3f58e67f98183349e801
Filipe Mota <filipera3@gmail.com>
2025-05-26 08:53:43 -0300

    feat: add method findByEmail in repository

--- src/main/java/com/ofilipecode/wish_list/repository/UserRepository.java
+++ src/main/java/com/ofilipecode/wish_list/repository/UserRepository.java
@@ -1,6 +1,12 @@
 package com.ofilipecode.wish_list.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.ofilipecode.wish_list.model.User;
+
 public interface UserRepository extends JpaRepository {
+
+    Optional<User> findByEmail(String email);
 }
