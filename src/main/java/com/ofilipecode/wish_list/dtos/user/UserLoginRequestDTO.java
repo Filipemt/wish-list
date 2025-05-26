d4dc1902bce920a17b7dc33d77e378ee6ee9798d
Filipe Mota <filipera3@gmail.com>
2025-05-26 08:20:27 -0300

    feat: add login and register DTO

---
+++ src/main/java/com/ofilipecode/wish_list/dtos/user/UserLoginRequestDTO.java
@@ -0,0 +1,4 @@
+package com.ofilipecode.wish_list.dtos.user;
+
+public record UserLoginRequestDTO(String email, String password) {
+}
---
+++ src/main/java/com/ofilipecode/wish_list/dtos/user/UserRegisterDTO.java
@@ -0,0 +1,4 @@
+package com.ofilipecode.wish_list.dtos.user;
+
+public record UserRegisterDTO(String name, String email, String password) {
+}
