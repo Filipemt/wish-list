195fab0bbec5a103c167fc652d0a7703867ccad5
Filipe Mota <filipera3@gmail.com>
2025-05-27 13:48:21 -0300

    feat: add update endpoint and add UserUpdateDTO

--- src/main/java/com/ofilipecode/wish_list/controller/UserController.java
+++ src/main/java/com/ofilipecode/wish_list/controller/UserController.java
@@ -3,12 +3,16 @@ package com.ofilipecode.wish_list.controller;
 import java.util.UUID;
 
 import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ofilipecode.wish_list.dtos.user.UserUpdateDTO;
 import com.ofilipecode.wish_list.dtos.user.UserResponseDTO;
 import com.ofilipecode.wish_list.service.UserService;
 
@@ -20,6 +24,17 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
 
     private final UserService service;
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Object> updateUser(@PathVariable String id,
+                                            @RequestBody UserUpdateDTO dto) {
+
+        UUID userID = UUID.fromString(id);
+        service.updateUser(userID, dto); 
+
+        return ResponseEntity.noContent().build();
+}
+
     
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDTO> getUserById(@PathVariable String id) {
--- src/main/java/com/ofilipecode/wish_list/dtos/user/UserUpdateDTO.java
+++ src/main/java/com/ofilipecode/wish_list/dtos/user/UserUpdateDTO.java
@@ -1,6 +1,4 @@
 package com.ofilipecode.wish_list.dtos.user;
 
-
-
 public record UserUpdateDTO(String name, String email, String password) {
 }
