1218c39326e553492ff478a851e7fa1c658138ce
Filipe Mota <filipera3@gmail.com>
2025-05-28 13:21:43 -0300

    feat: add Wish repository

---
+++ src/main/java/com/ofilipecode/wish_list/repository/WishRepository.java
@@ -0,0 +1,11 @@
+package com.ofilipecode.wish_list.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ofilipecode.wish_list.model.Wish;
+
+public interface WishRepository extends JpaRepository<Wish, UUID> {
+    
+}
